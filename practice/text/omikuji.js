ffd80ed1d0afb31f69a9df99e91ae258ff36d645
atmosphere24 <100392959+atmosphere24@users.noreply.github.com>
2022-03-06 16:03:50 +0900

    index.htmlとomikuji.jsの訂正
    「さん！」をindex.htmlの方に記入し、omikuji.jsにnameという名前の変数を定義した。

--- practice/text/omikuji.js
+++ practice/text/omikuji.js
@@ -1,23 +1,24 @@
+let name;
 let username;
 let userresult;
-let answer = window.prompt("お名前を教えてください。");
-if (answer == ''){
-    username = "名無しさん！"
+let answer = prompt("お名前を教えてください。");
+if(answer == ''){
+    username = "名無し"
 }else{
-    username = answer + "さん！"
+    username = answer
 }
 document.getElementById("name").innerHTML = username;
 
-let rand = Math.floor(Math.random()*5);
-if (rand == 0){
+let rand = Math.floor( Math.random() *5);
+if(rand == 0){
     userresult = "大吉";
 }else if(rand == 1){
-    userresult ="中吉";
+    userresult = "中吉";
 }else if(rand == 2){
-    userresult ="小吉";
+    userresult = "小吉";
 }else if(rand == 3){
-    userresult ="吉";
+    userresult = "吉";
 }else if(rand == 4){
     userresult ="凶";
 }
-documet.getElementById("result").innerHTML = userresult;
+document.getElementById("result").innerHTML = userresult;
